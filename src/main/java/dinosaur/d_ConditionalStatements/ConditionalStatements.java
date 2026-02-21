0e44a05d67164151cbdd548251dc6f911c3c9c1a
Giuseppe Berardi <berardig443@gmail.com>
2026-02-21 16:51:23 +0100

    fixed

---
+++ src/main/java/dinosaur/d_ConditionalStatements/ConditionalStatements.java
@@ -0,0 +1,23 @@
+package dinosaur.d_ConditionalStatements;
+
+import java.util.Scanner;
+
+public class ConditionalStatements {
+    public static void main (String [] args ) {
+        Scanner tastiera  = new Scanner(System.in);
+        System.out.println("scrivi un numero ");
+        String name = tastiera.nextLine();
+        System.out.println ("il tuo numero è " + name);
+
+
+        int[] numerini = new int[10];
+        numerini[0] = 1;
+        numerini[1] = 3+4;
+        numerini[2] = 2+3;
+
+
+
+
+
+    }
+}
